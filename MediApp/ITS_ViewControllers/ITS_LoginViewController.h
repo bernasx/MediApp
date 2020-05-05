750bb603afeb281c7e649d07ed0c329ace869068
Bernardo Miguel Nunes Ribeiro <bernardo.ribeiro99@gmail.com>
2020-05-05 12:01:27 +0100

    Storyboard Moving Test

---
+++ MediApp/ITS_ViewControllers/ITS_LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  ITS_LoginViewController.h
+//  MediApp
+//
+//  Created by Bernardo Miguel Nunes Ribeiro on 05/05/2020.
+//  Copyright © 2020 Bernardo Miguel Nunes Ribeiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ITS_LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MediApp/ITS_ViewControllers/ITS_LoginViewController.m
@@ -0,0 +1,32 @@
+//
+//  ITS_LoginViewController.m
+//  MediApp
+//
+//  Created by Bernardo Miguel Nunes Ribeiro on 05/05/2020.
+//  Copyright © 2020 Bernardo Miguel Nunes Ribeiro. All rights reserved.
+//
+
+#import "ITS_LoginViewController.h"
+
+@interface ITS_LoginViewController ()
+
+@end
+
+@implementation ITS_LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
